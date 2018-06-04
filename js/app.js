5a8aa1e83cdf9cecfd2615946cdd86e937b74976
Baruch Elias <eyesweyedopen@gmail.com>
2018-06-04 13:25:57 -0400

    Add comment for animation

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector(".container").addEventListener("click", function(e){
 		e.preventDefault();
 		e.stopPropagation();
+		// prevent container from animating
 		if (e.target != document.querySelector(".container")) {
 			e.target.classList.toggle("flip");
 		};
