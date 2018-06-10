33c0c6122bdc86f17c8f2c479f350d51f84b1971
Baruch Elias <eyesweyedopen@gmail.com>
2018-06-10 13:56:14 -0400

    Add timer functionality with timer() and pad() functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
+
+	document.getElementById("timer").innerHTML = "<p>00:00</p>";
+
 	document.querySelector(".container").addEventListener("click", function(e){
 		e.preventDefault();
 		e.stopPropagation();
@@ -9,15 +12,35 @@ document.addEventListener("DOMContentLoaded", function() {
 			e.target.classList.toggle("flip");
 		};
 	});
-})
+	// initiate timer
+	document.querySelector(".container").addEventListener("click", function(e){
+		timer();
+	}, {once: true});
+});
 
 function shuffleCards() {
 	// shuffle the cards
 };
 
 function timer() {
-	// create a working timer
+	const timer = document.getElementById("timer");
+
+	const startTime = new Date().getTime();
+
+	setInterval(function() {
+		const curTime = new Date().getTime();
+
+		const minutes = Math.floor( ( (curTime - startTime) % (1000 * 60 * 60) ) / (1000 * 60) );
+		const seconds = Math.floor( ( (curTime - startTime) % (1000 * 60) ) / 1000);
+
+		timer.innerHTML = `<p>${pad(minutes)}:${pad(seconds)}</p>`
+	}, 1000);
 };
+
+function pad(num) {
+	num = "0".repeat(2 - num.toString().length) + num.toString();
+	return num;
+}
 
 function moveCounter() {
 	// keeps track of your moves
